ed87370800cf74f7703a4aeb94c73ab4773358ee
yuta-komura <you@example.com>
2020-10-08 08:54:48 +0900

    変更 tvに合わせる

--- backtest.py
+++ backtest.py
@@ -24,9 +24,9 @@ sql = """
         WHERE
             (minute(Date)  = 55
         or  minute(Date) between 0 and 1
-        or  minute(Date) = 29)
-        # and
-            # Date >= '2020-9-15 00:00:00'
+        or  minute(Date) = 28)
+        and
+            Date >= '2020-9-15 00:00:00'
         order by
             Date
     """
@@ -56,7 +56,7 @@ for i in range(len(historical_Price)):
 
             tD = past_Date + datetime.timedelta(hours=1)
             if tD.hour != now_Date.hour or now_Date.hour != entry_Date.hour or entry_Date.hour != close_Date.hour \
-                    or past_Date.minute != 55 or entry_Date.minute != 1 or close_Date.minute != 29:
+                    or past_Date.minute != 55 or entry_Date.minute != 1 or close_Date.minute != 28:
                 continue
 
             past_Close = past_data["Close"]
