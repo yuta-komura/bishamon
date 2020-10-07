75c6d95ef7d4a38a458008cd8e773e011f9c2358
yuta-komura <you@example.com>
2020-10-07 18:48:06 +0900

    変更 テーブル定義

--- db.sql
+++ db.sql
@@ -15,18 +15,6 @@ CREATE TABLE `position` (
     `size` float NOT NULL
 );
 
-CREATE TABLE `child_order` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `date` datetime(6) NOT NULL,
-    `side` varchar(255) DEFAULT NULL,
-    `price` int unsigned DEFAULT NULL,
-    `size` float unsigned DEFAULT NULL,
-    `event_type` varchar(255) NOT NULL,
-    `child_order_type` varchar(255) DEFAULT NULL,
-    `product_code` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-
 CREATE TABLE `execution_history` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     `date` datetime(6) NOT NULL,
