abcc6f0532a084733e548aac8386f4eb3fd84572
yuta-komura <example@example.com>
2021-05-31 20:31:43 +0900

    Update indicator.py ラベル指定機能

--- lib/indicator.py
+++ lib/indicator.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def add_rsi(df: pd.DataFrame, value: int) -> pd.DataFrame:
-    price_diff_df = df["price"].diff()
+def add_rsi(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
+    price_diff_df = df[use_columns].diff()
 
     up = price_diff_df.copy()
     down = price_diff_df.copy()
@@ -16,14 +16,14 @@ def add_rsi(df: pd.DataFrame, value: int) -> pd.DataFrame:
     return df
 
 
-def add_sma(df: pd.DataFrame, value: int) -> pd.DataFrame:
-    price_df = df["price"]
+def add_sma(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
+    price_df = df[use_columns]
     df["sma{value}".format(value=value)] = price_df.rolling(value).mean()
     return df
 
 
-def add_ema(df: pd.DataFrame, value: int) -> pd.DataFrame:
-    price_df = df["price"]
+def add_ema(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
+    price_df = df[use_columns]
     sma = price_df.rolling(value).mean()[:value]
     df["ema{value}".format(value=value)] = \
         pd.concat([sma, price_df[value:]]).ewm(span=value, adjust=False).mean()
