c19f1103e3fbba75c37a324b0e0be9879518f350
yuta-komura <example@example.com>
2022-03-14 04:41:21 +0900

    update indicator.py add_bb

--- lib/indicator.py
+++ lib/indicator.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 
 def add_rsi(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
-    price_diff_df = df[use_columns].diff()
+    d = df.copy()
+    price_diff_df = d[use_columns].diff()
 
     up = price_diff_df.copy()
     down = price_diff_df.copy()
@@ -12,19 +13,32 @@ def add_rsi(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
     up_sum = up.rolling(value).sum()
     down_sum = down.abs().rolling(value).sum()
 
-    df["rsi"] = up_sum / (up_sum + down_sum) * 100
-    return df
+    d[f"rsi{use_columns}{value}"] = up_sum / (up_sum + down_sum) * 100
+    return d
 
 
 def add_sma(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
-    price_df = df[use_columns]
-    df["ma{value}".format(value=value)] = price_df.rolling(value).mean()
-    return df
+    d = df.copy()
+    price_df = d[use_columns]
+    d[f"sma{use_columns}{value}"] = price_df.rolling(value).mean()
+    return d
 
 
 def add_ema(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
-    price_df = df[use_columns]
+    d = df.copy()
+    price_df = d[use_columns]
     sma = price_df.rolling(value).mean()[:value]
-    df["ma{value}".format(value=value)] = \
+    d[f"ema{use_columns}{value}"] = \
         pd.concat([sma, price_df[value:]]).ewm(span=value, adjust=False).mean()
-    return df
+    return d
+
+
+def add_bb(df: pd.DataFrame, value: int, use_columns: str) -> pd.DataFrame:
+    d = df.copy()
+    mean = d[use_columns].rolling(window=value).mean()
+    std = d[use_columns].rolling(window=value).std()
+    upper = mean + (std * 2)
+    lower = mean - (std * 2)
+    d[f"bbupper{use_columns}{value}"] = upper
+    d[f"bblower{use_columns}{value}"] = lower
+    return d
